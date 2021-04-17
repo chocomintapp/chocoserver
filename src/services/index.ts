4ffe492daeb250c90e8958a9b27d8d63f7ee429b
ookimaki <noreply@ookimaki.com>
2021-04-17 22:12:32 +0900

    update node url

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,9 +3,11 @@ import configs from "../configs.json";
 
 export const sync = async () => {
   console.log("test");
-  const jsonRpcProvider = new ethers.providers.JsonRpcProvider(configs.rpc.mainnet);
+  const jsonRpcProvider = new ethers.providers.JsonRpcProvider(configs.rpc.bsc);
   const blockNumber = await jsonRpcProvider.getBlockNumber();
   console.log(blockNumber);
+  // const blockWithTransactions = await jsonRpcProvider.getBlockWithTransactions(9837019);
+  // console.log(blockWithTransactions);
 };
 
 sync();
