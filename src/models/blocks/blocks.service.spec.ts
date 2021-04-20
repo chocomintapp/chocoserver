1db0f43f1d7fde937eb00515de6a5d04127925e6
ookimaki <noreply@ookimaki.com>
2021-04-20 23:44:43 +0900

    chore: snapshot success service test

--- src/models/blocks/blocks.service.spec.ts
+++ src/models/blocks/blocks.service.spec.ts
@@ -1,15 +1,29 @@
-import { Test, TestingModule } from "@nestjs/testing";
+import { Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+
 import { BlocksService } from "./blocks.service";
+import { BlocksModule } from "./blocks.module";
+import { Block } from "./entities/block.entity";
 
 describe("BlocksService", () => {
   let service: BlocksService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [BlocksService],
-    }).compile();
+  const mockBlock = new Block();
 
-    service = module.get<BlocksService>(BlocksService);
+  beforeEach(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [BlocksModule],
+    })
+      .overrideProvider(getRepositoryToken(Block))
+      .useValue({
+        find: () => mockBlock,
+        findOne: () => mockBlock,
+        save: () => mockBlock,
+      })
+      .compile();
+    service = moduleFixture.get<BlocksService>(BlocksService);
+    const app = moduleFixture.createNestApplication();
+    await app.init();
   });
 
   it("should be defined", () => {
